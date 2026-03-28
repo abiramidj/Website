e710fe4cec93c1aa862d4f9197e14e2fe535fdf6
Abirami V Jegadeesh <avj@MacBook-Air.local>
2026-03-28 11:16:47 -0400

    auth and api config file

--- api/[...path].js
+++ api/[...path].js
@@ -1,4 +1,9 @@
 module.exports = async (req, res) => {
-  const { default: app } = await import('../server/index.js');
-  app(req, res);
+  try {
+    const { default: app } = await import('../server/index.js');
+    app(req, res);
+  } catch (err) {
+    console.error('[vercel-fn] import error:', err.message);
+    res.status(500).json({ error: err.message });
+  }
 };
