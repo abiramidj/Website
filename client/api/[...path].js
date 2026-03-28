f2d57ebbefef083a6a20c3ba2892e6dc9a50d868
Abirami V Jegadeesh <avj@MacBook-Air.local>
2026-03-28 11:21:28 -0400

    vercel config file

---
+++ client/api/[...path].js
@@ -0,0 +1,9 @@
+module.exports = async (req, res) => {
+  try {
+    const { default: app } = await import('../../server/index.js');
+    app(req, res);
+  } catch (err) {
+    console.error('[vercel-fn] error:', err.message);
+    res.status(500).json({ error: err.message });
+  }
+};
